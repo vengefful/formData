8cff162b34cb84306b5153793474ccf94f1887f5
Fernando <linguacavalcante@gmail.com>
2020-04-07 23:51:43 -0300

    updating

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -8,15 +8,15 @@ import json
 cgitb.enable()
 
 #  print("Content-Type: text/plain\n\n") # HTTP header to say HTML is following
-print "Content-type:text/html\r\n\r\n"
-print '<html>'
-print '<head>'
-print '<title>Hello World - First CGI Program</title>'
-print '</head>'
-print '<body>'
-print '<h2>Hello World! This is my first CGI program</h2>'
-print '</body>'
-print '</html>'
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title>Hello World - First CGI Program</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello World! This is my first CGI program</h2>')
+print ('</body>')
+print ('</html>')
 
 formData = cgi.FieldStorage()
 jsonData = formData.getvalue("foo")
