ccfc4f4fbc18ee149676026b4399666b41062fd6
Fernando <linguacavalcante@gmail.com>
2020-04-07 21:32:01 -0300

    updating

---
+++ cgi-bin/form.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+ 
+import cgi
+import requests
+import json
+
+print("Content-Type: text/html; charset=utf-8") # HTTP header to say HTML is following
+print()                          # blank line, end of headers
+
+formData = cgi.FieldStorage()
+jsonData = formData.getvalue("foo")
+data = json.loads(jsonData)
+
+for key in data:
+    aluno = key
+
+try:
+    with open("Testes.json") as file:
+        test = json.load(file)
+        if aluno in test.keys():
+            print("SAFADINHO, TA REPETINDO O TESTE NE SAFADO!!!! TE  PEGUEI MANE")
+        else:
+            data.update(test)
+            with open("Testes.json", 'w') as file:
+                json.dump(data, file, indent=2)
+            print("Resposta enviada com sucesso")
+except IOError:
+    with open("Testes.json", 'w') as file:
+        json.dump(data, file, indent=2)
+    print("Resposta enviada com sucesso!")
+except Exception as e:
+    print("Erro enviando resultados, por favor, reenvie!")
+
+
+#  try:
+    #  with open("Testes.json", 'a+') as file:
+        #  json.dump(data, file, indent=2)
+    #  print("Resposta enviada")
+#  except Exception as e:
+    #  print(e)
