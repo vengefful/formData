c3cf8d73ec8d89bff0733945dde24f91712e13e6
Fernando <linguacavalcante@gmail.com>
2020-04-08 03:37:17 -0300

    updating

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -23,7 +23,7 @@ data = json.loads(jsonData)
 for key in data:
     aluno = key
 
-filename = '../' + aluno['Serie'] + 'Tests.json' 
+filename = '../' + data[aluno]['Serie'] + 'Tests.json' 
 
 try:
     with open(filename) as file:
