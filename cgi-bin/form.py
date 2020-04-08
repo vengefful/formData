ae5bd552c5b8b5fa5ffc42a346ccab944f52cb79
Fernando <linguacavalcante@gmail.com>
2020-04-07 22:53:30 -0300

    updating

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python
  
-import cgi
-import requests
-import json
+#  import cgi
+#  import requests
+#  import json
 
 print("Content-Type: text/html; charset=utf-8") # HTTP header to say HTML is following
 print()                          # blank line, end of headers
+print("TESTANDO")
 
-formData = cgi.FieldStorage()
-jsonData = formData.getvalue("foo")
-data = json.loads(jsonData)
+#  formData = cgi.FieldStorage()
+#  jsonData = formData.getvalue("foo")
+#  data = json.loads(jsonData)
 
-for key in data:
-    aluno = key
+#  for key in data:
+    #  aluno = key
 
-try:
-    with open("Testes.json") as file:
-        test = json.load(file)
-        if aluno in test.keys():
-            print("SAFADINHO, TA REPETINDO O TESTE NE SAFADO!!!! TE  PEGUEI MANE")
-        else:
-            data.update(test)
-            with open("Testes.json", 'w') as file:
-                json.dump(data, file, indent=2)
-            print("Resposta enviada com sucesso")
-except IOError:
-    with open("Testes.json", 'w') as file:
-        json.dump(data, file, indent=2)
-    print("Resposta enviada com sucesso!")
-except Exception as e:
-    print("Erro enviando resultados, por favor, reenvie!")
+#  try:
+    #  with open("Testes.json") as file:
+        #  test = json.load(file)
+        #  if aluno in test.keys():
+            #  print("SAFADINHO, TA REPETINDO O TESTE NE SAFADO!!!! TE  PEGUEI MANE")
+        #  else:
+            #  data.update(test)
+            #  with open("Testes.json", 'w') as file:
+                #  json.dump(data, file, indent=2)
+            #  print("Resposta enviada com sucesso")
+#  except IOError:
+    #  with open("Testes.json", 'w') as file:
+        #  json.dump(data, file, indent=2)
+    #  print("Resposta enviada com sucesso!")
+#  except Exception as e:
+    #  print("Erro enviando resultados, por favor, reenvie!")
 
 
 #  try:
