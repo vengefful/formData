95bc01eac591067df8b66e068ad376658146d34f
Fernando <linguacavalcante@gmail.com>
2020-04-08 00:44:17 -0300

    updating

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -28,7 +28,7 @@ try:
     with open("../9ATestes.json") as file:
         test = json.load(file)
         if aluno in test.keys():
-            print('<p>ESTÁ REPETINDO O TESTE.</p>')
+            print('<p>REPETINDO O TESTE.</p>')
         else:
             data.update(test)
             with open("../9ATestes.json", 'w') as file:
