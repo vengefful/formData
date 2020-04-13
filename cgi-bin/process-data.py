e2e04f6cfab5733d8b0327ad407761e5b2b0ff8d
Fernando Jorge <45715067+vengefful@users.noreply.github.com>
2020-04-12 23:41:59 -0300

    updating lista 02

--- cgi-bin/process-data.py
+++ cgi-bin/process-data.py
@@ -13,6 +13,7 @@ html_header = """<!DOCTYPE html>
     <html lang="pt-br">
 
     <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Matemática</title>
     </head>
